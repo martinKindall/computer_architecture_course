9286684e3d67311b94cfcf53a3923914d2000780
martinKindall <martin@rflex.cl>
2018-10-14 13:20:15 -0300

    Se empieza T1

---
+++ tarea1/t1.asm
@@ -0,0 +1,65 @@
+.data 
+	mensajeA: .asciiz "Ingrese variable A: "
+	mensajeB: .asciiz "Ingrese variable B: "
+	mensajeC: .asciiz "Ingrese variable C: "
+
+	resultadoX: .asciiz "Resultado para X: "
+
+
+.text
+	la $a0, mensajeA
+	li $v0, 4		# print string
+	syscall
+
+# ingresar A
+	li $v0, 5
+	syscall
+
+# guardar A en $s3
+	add $s3, $v0, $zero
+
+	la $a0, mensajeB
+	li $v0, 4		# print string
+	syscall
+
+# ingresar B
+	li $v0, 5
+	syscall
+
+# guardar B en $s4
+	add $s4, $v0, $zero
+
+	la $a0, mensajeC
+	li $v0, 4		# print string
+	syscall
+
+# ingresar C
+	li $v0, 5
+	syscall
+
+# guardar C en $s5
+	add $s5, $v0, $zero
+
+	la $a0, resultadoX
+	li $v0, 4		# print string
+	syscall
+
+	jal rutinaX
+
+	add $a0, $v0, $zero
+	li $v0, 1		# print string
+	syscall
+
+
+rutinaX:	sll $t0, $s3, 1
+			sll $t1, $s4, 3
+			sll $t2, $s5, 2
+
+			#complemento de 4C
+			nor $t2, $t2, $zero
+			addi $t2, $t2, 1
+
+			add $v0, $t0, $t1
+			add $v0, $v0, $t2
+
+			jr $ra
