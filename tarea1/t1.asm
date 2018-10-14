7bf54d95b290f68116b168e4bdce3789cda5aa8d
martinKindall <martin@rflex.cl>
2018-10-14 13:56:08 -0300

    Se termina T1, faltan comentarios del codigo

--- tarea1/t1.asm
+++ tarea1/t1.asm
@@ -3,7 +3,11 @@
 	mensajeB: .asciiz "Ingrese variable B: "
 	mensajeC: .asciiz "Ingrese variable C: "
 
+	saltoLinea: .asciiz "\n"
+
 	resultadoX: .asciiz "Resultado para X: "
+	resultadoY: .asciiz "Resultado para Y: "
+	resultadoZ: .asciiz "Resultado para Z: "
 
 
 .text
@@ -40,11 +44,46 @@
 # guardar C en $s5
 	add $s5, $v0, $zero
 
+# salto de linea
+	la $a0, saltoLinea
+	li $v0, 4		# print string
+	syscall
+
 	la $a0, resultadoX
 	li $v0, 4		# print string
 	syscall
 
 	jal rutinaX
+
+	add $a0, $v0, $zero
+	li $v0, 1		# print int
+	syscall
+
+# salto de linea
+	la $a0, saltoLinea
+	li $v0, 4		# print string
+	syscall
+
+	la $a0, resultadoY
+	li $v0, 4		# print string
+	syscall
+
+	jal rutinaY
+
+	add $a0, $v0, $zero
+	li $v0, 1		# print int
+	syscall
+
+# salto de linea
+	la $a0, saltoLinea
+	li $v0, 4		# print string
+	syscall
+
+	la $a0, resultadoZ
+	li $v0, 4		# print string
+	syscall
+
+	jal rutinaZ
 
 	add $a0, $v0, $zero
 	li $v0, 1		# print int
@@ -66,4 +105,33 @@ rutinaX:	sll $t0, $s3, 1
 
 			jr $ra
 
-exit: 
+
+rutinaY:	sll $t0, $s3, 2
+			sll $t1, $s4, 5
+			sll $t2, $s5, 3
+
+			#complemento de 32B
+			nor $t1, $t1, $zero
+			addi $t1, $t1, 1
+
+			add $v0, $t0, $t1
+			add $v0, $v0, $t2
+
+			jr $ra
+
+
+rutinaZ:	sll $t0, $s3, 3
+			sll $t1, $s4, 4
+			sll $t2, $s5, 5
+
+			#complemento de 32B
+			nor $t2, $t2, $zero
+			addi $t2, $t2, 1
+
+			add $v0, $t0, $t1
+			add $v0, $v0, $t2
+
+			jr $ra
+
+
+exit:
